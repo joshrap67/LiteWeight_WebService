fc05c6b633511cc467937cbc06c57ba0ed64962b
joshrap67 <ghosthippo67@gmail.com>
2020-12-04 18:03:30 -0500

    Got rid of bloated package and have a sexy new script for auto confirm users

--- auto_confirm_users/build_lambda_package.bat
+++ auto_confirm_users/build_lambda_package.bat
@@ -1,1 +1,5 @@
-tar -cf lambda_package.zip "C:\Users\Josh\github\LiteWeight_WebService\auto_confirm_users\Lib\site-packages"
+if not exist .\site-packages\ pip install -r requirements.txt -t .\site-packages
+xcopy /y .\auto_confirm.py .\site-packages
+cd site-packages
+7z.exe a lambda_package.zip .
+MOVE /Y .\lambda_package.zip ..
